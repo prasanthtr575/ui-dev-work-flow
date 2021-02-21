6ab4ab9169618a869bf62bfe21ead09e4fdf14b9
Prasanth Raghavan <mailprasanthtr@gmail.com>
2021-02-21 23:31:39 +0530

    Add validation to the toolbar inputbox

--- src/components/molecues/toolbar/Toolbar.tsx
+++ src/components/molecues/toolbar/Toolbar.tsx
@@ -4,11 +4,22 @@ import TextField from '../../atoms/textField/TextField';
 
 function Toolbar({ addTask }: any) {
   const [task, setTask] = useState('');
+  const BTN_ENABLED_CLS =
+    'hover:bg-primary hover:text-light bg-light text-primary text-label font-bold border border-primary px-57px py-20px ml-20px';
+  const BTN_DISABLED_CLS =
+    'bg-tertiary text-light text-label font-bold px-57px py-20px ml-20px cursor-not-allowed';
+  const [btnEnabled, setBtnEnabled] = useState(false);
+
   const onChange = (e: any) => {
-    setTask(e.target.value);
+    const text = e.target.value;
+
+    setBtnEnabled(text.length > 0);
+    setTask(text);
   };
   const onAdd = () => {
     addTask instanceof Function && addTask(task);
+    setTask('');
+    setBtnEnabled(false);
   };
 
   return (
@@ -20,8 +31,9 @@ function Toolbar({ addTask }: any) {
         onChange={onChange}
       />
       <Button
-        className="hover:bg-primary hover:text-light bg-light text-primary text-label font-bold border border-primary px-57px py-20px ml-20px"
+        className={btnEnabled ? BTN_ENABLED_CLS : BTN_DISABLED_CLS}
         onClick={onAdd}
+        disabled={!btnEnabled}
       >
         Add
       </Button>
